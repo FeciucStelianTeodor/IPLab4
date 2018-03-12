02fb1918c82ebf6f96cbe1fa4eb676b862c7328b
FeciucStelianTeodor <feciucstelian@gmail.com>
2018-03-12 14:50:19 +0200

    S-a adaugat clasa Persoana

---
+++ IpLab4/src/iplab4/Persoana.java
@@ -0,0 +1,29 @@
+public abstract class Persoana {
+    String nume;
+    String email;
+    String cont;
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCont() {
+        return cont;
+    }
+
+    public void setCont(String cont) {
+        this.cont = cont;
+    }
+}
