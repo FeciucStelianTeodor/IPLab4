5e71661ca45502cb13c00fe2627314cba3dba013
Mihai Bojescu <mihaibojescu@atlassian.com>
2018-03-12 15:40:10 +0200

    Adaugat login din terminal

--- IpLab4/src/iplab4/IpLab4.java
+++ IpLab4/src/iplab4/IpLab4.java
@@ -5,6 +5,10 @@
  */
 package iplab4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author feciu
@@ -17,18 +21,35 @@ public class IpLab4 {
     public static void main(String[] args) {
         //Construim un server
         Server server = new Server();
+
+        String[] credentials = new String[3];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Nume: ");
+            credentials[0] = reader.readLine();
+            System.out.println("Email: ");
+            credentials[1] = reader.readLine();
+            System.out.println("Cont: ");
+            credentials[2] = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         
-        //Construim o persoană
+                //Construim o persoană
         Persoana persoana = new Persoana();
-        persoana.setNume("Gheorghe");
-        persoana.setEmail("gheorghe@lazar.ro");
-        persoana.setCont("gheorghe");
-        
+        persoana.setNume(credentials[0]);
+        persoana.setEmail(credentials[1]);
+        persoana.setCont(credentials[2]);
+
         //Verificăm dacă persoana este validă
-        if(server.doLogare(persoana))
-        {
+        if (server.doLogare(persoana)) {
             System.out.println("Logat " + persoana);
         }
+        else {
+            System.out.println("Nu s-a putut autentifica utilizatorul");
+        }
+
+
     }
-    
+
 }
